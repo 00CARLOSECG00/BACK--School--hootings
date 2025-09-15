5643f5cb16062de47b8de200241dc63ad1408b5c
00CARLOSECG00 <160240864+00CARLOSECG00@users.noreply.github.com>
2025-09-15 01:43:31 -0500

    First commit

---
+++ incidents-api/src/test/java/com/example/incidents_api/IncidentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.incidents_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
